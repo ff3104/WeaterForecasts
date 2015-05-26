7130be85f2cf6de9c4297e3a57db91644150391d
Satoshi Muranaka <ff3104@gmail.com>
2015-05-26 18:01:58 +0900

    単一ページに地域、今日、明日、明後日の天気を表示させる

--- app/src/main/java/com/freefly3104/satoshi/weaterforecasts/WeatherForecast.java
+++ app/src/main/java/com/freefly3104/satoshi/weaterforecasts/WeatherForecast.java
@@ -12,7 +12,7 @@ import java.util.List;
  */
 public class WeatherForecast {
     public Location location;
-    public List<Forecast> forecastList= new ArrayList<>();
+    public List<Forecast> forecastList= new ArrayList();
 
     public WeatherForecast(JSONObject jsonObject) throws JSONException{
 
